987ed06737219a4f0d7a44d880a266433a256a4a
Naylimlia <nayliamalia42@gmail.com>
2025-05-28 14:08:57 +0800

    menambahkan file struct

---
+++ c030324049struct.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
+
+int main() {
+    mahasiswa nayli;
+    mahasiswa* ptr = &nayli;
+
+    (*ptr).nim = "c030324049";
+    (*ptr).nama = "Nayli Amalia";
+    (*ptr).alamat = "JL. Mahligai NO.66";
+    (*ptr).ipk = 3.9;
+    
+
+   cout << "nim:" << (*ptr).nim << endl;
+   cout << "nama:" << (*ptr).nama << endl;
+   cout << "alamat:" << (*ptr).alamat << endl;
+   cout << "ipk:" << (*ptr).ipk << endl;
+
+    return 0;
+}
